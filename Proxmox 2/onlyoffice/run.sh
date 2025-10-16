48a322f950a63030170b94f135090b33c8817a2c
Marcos Pablo Russo <marcospr1974@gmail.com>
2025-10-16 17:33:35 -0300

    Delete Proxmox 2/onlyoffice directory

--- Proxmox 2/onlyoffice/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/ONLYOFFICE/Docker-CommunityServer
-mv Docker-CommunityServer only-workspace
-cd only-workspace
-mv docker-compose.workspace.yml docker-compose.yml
-nano docker-compose.yml
-docker compose up -d
